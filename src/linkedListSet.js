9798e97294a12fa6920505c93576a6d60bee3de7
ngarciaotero <nasleygarciaotero@gmail.com>
2024-09-06 14:58:53 -0400

    linked list that handles nodes with only a key
    - linked list will be used with hashSetFactory.js to handle single key nodes

---
+++ src/linkedListSet.js
@@ -0,0 +1,153 @@
+import { node } from "./nodeSet.js";
+
+export function linkedList() {
+  let head = null;
+  let tail = null;
+  let size = 0;
+
+  // add node to end of list
+  function append(key) {
+    const newNode = node(key);
+    if (tail) {
+      tail.setNext(newNode);
+    } else {
+      head = newNode;
+    }
+    tail = newNode;
+    size++;
+  }
+
+  // add node to start of list
+  function prepend(key) {
+    const newNode = node(key);
+    if (head) {
+      newNode.setNext(head);
+    } else {
+      tail = newNode;
+    }
+    head = newNode;
+    size++;
+  }
+
+  // get list length
+  function getSize() {
+    return size;
+  }
+
+  // get first node
+  function getHead() {
+    return head;
+  }
+
+  // get last node
+  function getTail() {
+    return tail;
+  }
+
+  // get node at given index
+  function at(index) {
+    if (index >= size || index < 0) return null;
+
+    let currentNode = head;
+    for (let i = 0; i < index; i++) {
+      currentNode = currentNode.getNext();
+    }
+    return currentNode;
+  }
+
+  // remove last node
+  function pop() {
+    if (size === 0) return;
+
+    if (size === 1) {
+      head = null;
+      tail = null;
+    } else {
+      let secondToLast = at(size - 2);
+      secondToLast.setNext(null);
+      tail = secondToLast;
+    }
+    size--;
+  }
+
+  // remove first node
+  function shift() {
+    if (size === 0) return;
+
+    head = head.getNext();
+    if (size === 1) {
+      tail = null;
+    }
+    size--;
+  }
+
+  // checks if value exists
+  function contains(key) {
+    let currentNode = head;
+    while (currentNode) {
+      if (currentNode.getKey() === key) {
+        return true;
+      }
+      currentNode = currentNode.getNext();
+    }
+    return false;
+  }
+
+  // gets the index of found value
+  function find(key) {
+    let currentNode = head;
+    let i = 0;
+    while (currentNode) {
+      if (currentNode.getKey() === key) {
+        return i;
+      }
+      currentNode = currentNode.getNext();
+      i++;
+    }
+    return null;
+  }
+
+  // add value at given index
+  function insertAt(key, index) {
+    if (index === 0) {
+      prepend(key);
+    } else if (index === getSize()) {
+      append(key);
+    } else {
+      let newNode = node(key);
+      let previousNode = at(index - 1);
+      newNode.setNext(previousNode.getNext());
+      previousNode.setNext(newNode);
+      size++;
+    }
+  }
+
+  // remove value at given index
+  function removeAt(index) {
+    if (index === 0) {
+      shift();
+    } else if (index === getSize() - 1) {
+      pop();
+    } else {
+      let previousNode = at(index - 1);
+      let removeNode = previousNode.getNext();
+      previousNode.setNext(removeNode.getNext());
+      size--;
+    }
+  }
+
+  return {
+    append,
+    prepend,
+    getSize,
+    getHead,
+    getTail,
+    at,
+    pop,
+    shift,
+    contains,
+    find,
+    insertAt,
+    removeAt,
+  };
+}
